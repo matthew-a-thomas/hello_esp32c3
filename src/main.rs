a999b8f933ded12c01428a6ea667f6acd6a7e86e
Matt Thomas <me@matthewathomas.com>
2022-02-05 22:57:23 -0500

    Fix reading from `stdin()` but now it's failing to connect to the specified AP

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
 use embedded_svc::wifi::Wifi;
+use std::io::BufRead;
+use std::io::Read;
 use std::sync::Arc;
 
 type Error = Box<dyn std::error::Error>;
@@ -14,6 +16,9 @@ fn main() -> Result<!, Error> {
     println!("Hello, world!");
     
     let std_in = std::io::stdin();
+    let std_in = std_in.lock();
+    let std_in: BlockingReader<_> = std_in.into();
+    let mut std_in = std::io::BufReader::new(std_in);
     let if_stack = Arc::from(esp_idf_svc::netif::EspNetifStack::new()?);
     let sys_loop_stack = Arc::from(esp_idf_svc::sysloop::EspSysLoopStack::new()?);
     let nvs = Arc::from(esp_idf_svc::nvs::EspDefaultNvs::new()?);
@@ -31,10 +36,10 @@ fn main() -> Result<!, Error> {
     loop {
         println!("SSID?");
         let mut ssid = String::default();
-        std_in.read_line(&mut ssid).expect("Failed to read from stdin");
+        std_in.read_line(&mut ssid)?;
         println!("Password?");
         let mut password = String::default();
-        std_in.read_line(&mut password).expect("Failed to read from stdin");
+        std_in.read_line(&mut password)?;
 
         println!("Connecting...");
         wifi.set_configuration(&embedded_svc::wifi::Configuration::Client(embedded_svc::wifi::ClientConfiguration {
@@ -52,10 +57,44 @@ fn main() -> Result<!, Error> {
                 println!("Failed to connect.");
             },
         }
+
+        std::thread::sleep(core::time::Duration::from_secs(5));
     }
 
     loop {
-
+        println!("Sleeping...");
         std::thread::sleep(core::time::Duration::from_secs(5));
+    }
+}
+
+/// https://github.com/ivmarkov/rust-esp32-std-demo/issues/59
+struct BlockingReader<R: std::io::Read> {
+    poll: core::time::Duration,
+    reader: R,
+}
+
+impl<R: Read> From<R> for BlockingReader<R> {
+    fn from(reader: R) -> Self {
+        Self {
+            poll: core::time::Duration::from_millis(250),
+            reader,
+        }
+    }
+}
+
+impl<R: Read> std::io::Read for BlockingReader<R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if buf.len() == 0 {
+            return Ok(0);
+        }
+        loop {
+            match self.reader.read(buf) {
+                Ok(num_bytes) => return Ok(num_bytes),
+                Err(error) => match error.kind() {
+                    std::io::ErrorKind::WouldBlock => std::thread::sleep(self.poll),
+                    _ => return Err(error),
+                }
+            }
+        }
     }
 }

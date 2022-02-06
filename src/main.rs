aa4aca1822030090c952bf1aa45aea45325cd7c7
Matt Thomas <me@matthewathomas.com>
2022-02-06 16:26:07 -0500

    Sync system time with SNTP upon WiFi connection

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,58 @@ use std::sync::Arc;
 
 type Error = Box<dyn std::error::Error>;
 
+fn connect_wifi(
+    if_stack: Arc<esp_idf_svc::netif::EspNetifStack>,
+    sys_loop_stack: Arc<esp_idf_svc::sysloop::EspSysLoopStack>,
+    nvs: Arc<esp_idf_svc::nvs::EspDefaultNvs>,
+    std_in: &mut impl BufRead,
+) -> Result<esp_idf_svc::wifi::EspWifi, Error> {
+    loop {
+        let mut wifi = esp_idf_svc::wifi::EspWifi::new(if_stack.clone(), sys_loop_stack.clone(), nvs.clone())?;
+
+        println!("SSID?");
+        let mut ssid = String::default();
+        std_in.read_line(&mut ssid)?;
+        let ssid = ssid.trim().into();
+        println!("Password?");
+        let mut password = String::default();
+        std_in.read_line(&mut password)?;
+        let password = password.trim().into();
+
+        println!("Connecting to \"{}\"...", &ssid);
+        match wifi.set_configuration(&embedded_svc::wifi::Configuration::None) {
+            Ok(()) => {},
+            Err(err) => {
+                eprintln!("{}", err);
+                continue;
+            },
+        };
+        match wifi.set_configuration(&embedded_svc::wifi::Configuration::Client(embedded_svc::wifi::ClientConfiguration {
+            ssid,
+            password,
+            ..Default::default()
+        })) {
+            Ok(()) => {},
+            Err(err) => {
+                eprintln!("{}", err);
+                continue;
+            },
+        };
+        match wifi.get_status() {
+            embedded_svc::wifi::Status(embedded_svc::wifi::ClientStatus::Started(status), _) => {
+                println!("Connected!");
+                println!("{:?}", status);
+                return Ok(wifi);
+            },
+            _ => {
+                eprintln!("Failed to connect.");
+            },
+        }
+
+        std::thread::sleep(core::time::Duration::from_secs(5));
+    }
+}
+
 fn main() -> Result<!, Error> {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
@@ -23,54 +75,42 @@ fn main() -> Result<!, Error> {
     let sys_loop_stack = Arc::from(esp_idf_svc::sysloop::EspSysLoopStack::new()?);
     let nvs = Arc::from(esp_idf_svc::nvs::EspDefaultNvs::new()?);
 
-    let mut wifi = esp_idf_svc::wifi::EspWifi::new(if_stack, sys_loop_stack, nvs)?;
+    {
+        let mut wifi = esp_idf_svc::wifi::EspWifi::new(if_stack.clone(), sys_loop_stack.clone(), nvs.clone())?;
 
-    println!("Scanning for access points...");
-    let ap_infos = wifi.scan()?;
+        println!("Scanning for access points...");
+        let ap_infos = wifi.scan()?;
 
-    println!("Here they are:");
-    for ap_info in ap_infos.into_iter() {
-        println!("{:#?}", ap_info);
-    }
-
-    loop {
-        println!("SSID?");
-        let mut ssid = String::default();
-        std_in.read_line(&mut ssid)?;
-        let ssid = ssid.trim().into();
-        println!("Password?");
-        let mut password = String::default();
-        std_in.read_line(&mut password)?;
-        let password = password.trim().into();
-
-        println!("Connecting to \"{}\"...", &ssid);
-        match wifi.set_configuration(&embedded_svc::wifi::Configuration::Client(embedded_svc::wifi::ClientConfiguration {
-            ssid,
-            password,
-            ..Default::default()
-        })) {
-            Ok(()) => {},
-            Err(err) => {
-                eprintln!("{:?}", err);
-                continue;
-            },
-        };
-        match wifi.get_status() {
-            embedded_svc::wifi::Status(embedded_svc::wifi::ClientStatus::Started(status), _) => {
-                println!("Connected!");
-                println!("{:?}", status);
-                break;
-            },
-            _ => {
-                eprintln!("Failed to connect.");
-            },
+        println!("Here they are:");
+        for ap_info in ap_infos.into_iter() {
+            println!("{:#?}", ap_info);
         }
-
-        std::thread::sleep(core::time::Duration::from_secs(5));
     }
 
+    let _wifi = connect_wifi(if_stack.clone(), sys_loop_stack.clone(), nvs.clone(), &mut std_in)?;
+
+    let sntp = esp_idf_svc::sntp::EspSntp::new_default()?;
+    println!("Waiting for SNTP...");
     loop {
-        println!("Sleeping...");
+        let status = sntp.get_sync_status();
+        if status == esp_idf_svc::sntp::SyncStatus::Completed {
+            println!("Got SNTP!");
+            break;
+        }
+        let now = std::time::SystemTime::now();
+        let seconds = now.clone().duration_since(std::time::SystemTime::UNIX_EPOCH)?.as_secs();
+        let minutes = seconds / 60;
+        if minutes >= 5 {
+            eprintln!("Done waiting for SNTP");
+            break;
+        }
+        println!("{:?}\n{:?}", now, status);
+        std::thread::sleep(core::time::Duration::from_secs(1));
+    }
+    loop {
+        let now = std::time::SystemTime::now();
+        let now = now.duration_since(std::time::SystemTime::UNIX_EPOCH)?;
+        println!("{:?}", now);
         std::thread::sleep(core::time::Duration::from_secs(5));
     }
 }

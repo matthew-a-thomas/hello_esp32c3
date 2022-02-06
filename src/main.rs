fadceece017f99b7c934d3a80e0e169034d6c91b
Matt Thomas <me@matthewathomas.com>
2022-02-05 21:45:33 -0500

    (fail to) connect to SSID

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
 use embedded_svc::wifi::Wifi;
-use esp_idf_svc::{
-    netif::EspNetifStack,
-    nvs::EspDefaultNvs,
-    sysloop::EspSysLoopStack,
-    wifi::EspWifi,
-};
 use std::sync::Arc;
 
 type Error = Box<dyn std::error::Error>;
@@ -19,20 +13,48 @@ fn main() -> Result<!, Error> {
     
     println!("Hello, world!");
     
-    let if_stack = Arc::from(EspNetifStack::new()?);
-    let sys_loop_stack = Arc::from(EspSysLoopStack::new()?);
-    let nvs = Arc::from(EspDefaultNvs::new()?);
+    let std_in = std::io::stdin();
+    let if_stack = Arc::from(esp_idf_svc::netif::EspNetifStack::new()?);
+    let sys_loop_stack = Arc::from(esp_idf_svc::sysloop::EspSysLoopStack::new()?);
+    let nvs = Arc::from(esp_idf_svc::nvs::EspDefaultNvs::new()?);
 
-    let mut wifi = EspWifi::new(if_stack, sys_loop_stack, nvs)?;
+    let mut wifi = esp_idf_svc::wifi::EspWifi::new(if_stack, sys_loop_stack, nvs)?;
+
+    println!("Scanning for access points...");
+    let ap_infos = wifi.scan()?;
+
+    println!("Here they are:");
+    for ap_info in ap_infos.into_iter() {
+        println!("{:#?}", ap_info);
+    }
 
     loop {
-        println!("Scanning for access points...");
-        let ap_infos = wifi.scan()?;
+        println!("SSID?");
+        let mut ssid = String::default();
+        std_in.read_line(&mut ssid).expect("Failed to read from stdin");
+        println!("Password?");
+        let mut password = String::default();
+        std_in.read_line(&mut password).expect("Failed to read from stdin");
 
-        println!("Here they are:");
-        for ap_info in ap_infos.into_iter() {
-            println!("{:#?}", ap_info);
+        println!("Connecting...");
+        wifi.set_configuration(&embedded_svc::wifi::Configuration::Client(embedded_svc::wifi::ClientConfiguration {
+            ssid,
+            password,
+            ..Default::default()
+        }))?;
+        match wifi.get_status() {
+            embedded_svc::wifi::Status(embedded_svc::wifi::ClientStatus::Started(status), _) => {
+                println!("Connected!");
+                println!("{:?}", status);
+                break;
+            },
+            _ => {
+                println!("Failed to connect.");
+            },
         }
+    }
+
+    loop {
 
         std::thread::sleep(core::time::Duration::from_secs(5));
     }
